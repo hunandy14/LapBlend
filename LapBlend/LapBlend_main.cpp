3919b09f83c70928f288f3cf2eac2f9665494ae5
CharlotteHonG <hunandy14@gmail.com>
2018-04-19 19:53:56 +0800

    完成 1. 刪除多餘代碼 2. 修復偏差值 y +- 的錯誤

---
+++ LapBlend/LapBlend_main.cpp
@@ -0,0 +1,21 @@
+/*****************************************************************
+Name :
+Date : 2018/04/12
+By   : CharlotteHonG
+Final: 2018/04/12
+*****************************************************************/
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+#include <timer.hpp>
+#include "Raw2Img.hpp"
+#include "LapBlend.hpp"
+
+//================================================================
+int main(int argc, char const *argv[]) {
+	LapBlend_Tester();
+	return 0;
+}
+//================================================================
